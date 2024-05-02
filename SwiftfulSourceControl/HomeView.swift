589288955a52f7e583ec8c105fa51e95408bb59e
Christian Riest <christian.riest@cs.tu-dortmund.de>
2024-05-02 22:40:28 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Christian Riest on 02.05.24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
