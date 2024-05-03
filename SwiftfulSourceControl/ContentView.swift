d8db5912fb85f3fab5609b250d5aef2bf3fee019
Christian Riest <christian.riest@cs.tu-dortmund.de>
2024-05-03 19:19:09 +0200

    [Feature] Add rectangle to ContentView, tweaks to HomeView

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -38,6 +38,8 @@ struct ContentView: View {
                 
             }
             
+            Rectangle()
+            
         }
         .padding()
     }
--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -13,6 +13,7 @@ struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         ZStack{
+            Text("Hi")
             Text("Screen 2!")
             Text("Screen 2!")
             Text("Screen 2!")
